c16f313d4b46fb674779027240a0f27f37919c99
Brad <bradgc97@gmail.com>
2015-06-18 19:39:11 +1200

    Adding queue/threading support. Changed URL to list to support downloading more than one song. Added zip function.

--- soundDown.py
+++ soundDown.py
@@ -1,82 +1,115 @@
 import requests
 import json
 import mutagen
+import zipfile
+import os
 from bs4 import BeautifulSoup
 from clint.textui import progress
-
-
+from queue import Queue
+from threading import Thread
 
 
 class soundDown:
+    def Zip(self):
+        with zipfile.ZipFile("files.zip", "w") as f:
+            for file in os.listdir(os.curdir):
+                if ".mp3" in file:
+                    f.write(file)
 
-    def __init__(self):
+    def __download(self, q, url):
 
-        self.trackID = None
+        request = requests.session()
+        ID = self.__getTrakeId(url)
+        fullurl = "http://api.soundcloud.com/i1/tracks/{0}/streams?client_id=b45b1aa10f1ac2941910a7f0d10f8e28&app_version=8bae64e".format(
+            ID)
+        response = request.get(fullurl).text
+        j = json.loads(response)
+        link = j["http_mp3_128_url"]
+        if link is not None:
+            url = link
+        else:
+            raise Exception("Failed to get download link")
 
-    def download(self,url):
+        request = requests.session()
 
-        s =requests.session()
-        b = s.get(url,stream=True)
-        a, c = self.getTrackInfo()
+        response = request.get(url, stream=True)
+
+        a, c = self.__getTrackInfo(ID)
+
         filename = c + ".mp3"
 
         with open(filename, 'wb') as fd:
-            total_length = int(b.headers.get('content-length'))#taken from http://stackoverflow.com/a/20943461
-            for chunk in progress.bar(b.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+
+            total_length = int(response.headers.get('content-length'))  # taken from http://stackoverflow.com/a/20943461
+            for chunk in progress.bar(response.iter_content(chunk_size=1024), expected_size=(total_length / 1024)):
                 if chunk:
                     fd.write(chunk)
                     fd.flush()
+        filename = [filename, a, c]
 
-        filename = [filename,a,c]
-        self.addtags(filename)
+        self.__addtags(filename)
 
-    def addtags(self,filename):
+        q.task_done()
+
+    def __addtags(self, filename):
 
         try:
             audio = mutagen.EasyID3(filename[0])
         except Exception:
-            audio = mutagen.File(filename[0],easy=True)
+            audio = mutagen.File(filename[0], easy=True)
         finally:
             audio['title'] = filename[2]
             audio['artist'] = filename[1]
-            audio.save(filename[0],v1=2)
+            audio.save(filename[0], v1=2)
 
-    def getdownload(self,url):
 
-        s = requests.session()
-        self.trackID = self.getTrakeId(url)
-        work = "http://api.soundcloud.com/i1/tracks/{0}/streams?client_id=b45b1aa10f1ac2941910a7f0d10f8e28&app_version=8bae64e".format(self.trackID)
-        response = s.get(work).text
-        j = json.loads(response)
-        link = j["http_mp3_128_url"]
-        if link is not None:
-            self.download(link)
-        else:
-            raise Exception("test")
-     
-    def getTrakeId(self,url):
+    def __getTrakeId(self, url):
 
-        s = requests.session()
-        a = s.get(url).text
+        request = requests.session()
+        a = request.get(url).text
+
         soup = BeautifulSoup(a)
-        try:
-            names = soup.find("meta", {"property":"twitter:app:url:googleplay"})
-            names = names['content']
-            names = names.replace("soundcloud://sounds:","")
-            trakeid = names
 
-            return trakeid
+        try:
+            names = soup.find("meta", {"property": "twitter:app:url:googleplay"})
+            names = names['content']
+            names = names.replace("soundcloud://sounds:", "")
+            ID = names
+
+            return ID
+
         except Exception:
             raise BaseException("Not a valid track")
 
-    def getTrackInfo(self):
 
-        s = requests.session()
-        link = 'http://api.soundcloud.com/tracks/{0}.json?client_id=b45b1aa10f1ac2941910a7f0d10f8e28'.format(self.trackID)
-        response = s.get(link).text
+    def __getTrackInfo(self, ID):
+
+        request = requests.session()
+        link = 'http://api.soundcloud.com/tracks/{0}.json?client_id=b45b1aa10f1ac2941910a7f0d10f8e28'.format(ID)
+        response = request.get(link).text
 
         j = json.loads(response)
 
         artist = j["user"]["username"]
         title = j["title"]
-        return (artist,title)
+
+        return (artist, title)
+
+    def Download(self, url, Zip):
+
+        threads = len(url)
+        q = Queue(maxsize=0)
+
+        for i in range(threads):
+            worker = Thread(target=self.__download, args=(q, url[i]))
+            worker.setDaemon(True)
+            worker.start()
+
+        for i in url:
+            q.put(i)
+            print("Downloading %s" % i)
+
+        q.join()
+
+        if Zip:
+            self.Zip()
